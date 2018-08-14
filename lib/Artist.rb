c63976f64feb9e3ade27a0a3ecb0b853e6c15a87
Emily Jennings <emily.jennings@flatironschool.com>
2018-08-14 01:00:24 +0000

    Done.

---
+++ lib/Artist.rb
@@ -0,0 +1,32 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save
+    artist
+  end
+
+  def songs
+    @songs
+  end
+
+end
