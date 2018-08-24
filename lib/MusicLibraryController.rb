d25674411639217316a5550fd47a49c5b3471a34
Emily Jennings <emily.jennings@flatironschool.com>
2018-08-24 17:31:24 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -68,7 +68,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip
-    Song.all.sort{|a, b| a.name <=> b.name}
+    array = Song.all.sort{|a, b| a.name <=> b.name}
     i = input.to_i
   end
 end
