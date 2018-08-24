cb8aa7aa9e2fa01aba4fc90d0e0c63f6adaa4b7c
Emily Jennings <emily.jennings@flatironschool.com>
2018-08-24 17:30:38 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -46,9 +46,29 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
-    # artist = Artist.find_by_name(input)
-    # artist.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
-    #   puts "#{i+1}. #{s.name}"
-    # end
+
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
+        puts "#{i+1}. #{s.name} - #{s.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
+        puts "#{i+1}. #{s.artist.name} - #{s.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip
+    binding.pry
+    Song.all.sort{|a, b| a.name <=> b.name}
   end
 end
